a4ace8418fe0e01e01028292a2afc0c1f90ddc32
kayansama <lagrich.mohamed@outlook.com>
2024-12-27 18:15:14 +0100

    first commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,11 @@
+import { MdContactMail } from "react-icons/md";
+import { RiContactsBook3Line } from "react-icons/ri";
+
+export default function Index() {
+  return (
+    <div id="zero-state">
+      <MdContactMail size={50} color="orange" />
+      <h1>Contact App</h1>
+    </div>
+  );
+}
